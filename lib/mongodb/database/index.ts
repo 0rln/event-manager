56c72cdbae8ed1943f002c45d5a1fffbcf223d1b
Udai <udairelan@gmail.com>
2024-06-19 20:02:21 +0530

    chore: Fix typo in connectToDatabase function name

--- lib/mongodb/database/index.ts
+++ lib/mongodb/database/index.ts
@@ -5,7 +5,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // in case there is no mongoose cache, set it to an empty object
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
-export const connectToDatavase = async () => {
+export const connectToDatabase = async () => {
 	if (cached.conn) return cached.conn;
 
 	if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
