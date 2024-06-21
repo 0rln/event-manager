3e8d1bdf2285dc9564deb059271965d111942ad0
Udai <udairelan@gmail.com>
2024-06-21 23:36:35 +0530

    chore: Update CreateEvent page with background color and text alignment

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,25 +1,25 @@
 import EventForm from '@/components/shared/EventForm';
-import {auth} from '@clerk/nextjs/server';
-import React from 'react'
+import { auth } from '@clerk/nextjs/server';
+import React from 'react';
 
 const CreateEvent = () => {
-  const {sessionClaims} = auth()
+	const { sessionClaims } = auth();
 
-  const userId = sessionClaims?.userId as string
+	const userId = sessionClaims?.userId as string;
 
-  return (
+	return (
 		<>
-			<section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
-				<h3 className='wrapper h3-bold text-center sm:text-left'>
+			<section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10 bg-white'>
+				<h3 className='wrapper h3-bold text-center sm:text-left bg-transparent'>
 					Create Event
 				</h3>
 			</section>
 
-      <div className='wrapper my-8'>
-        <EventForm userId={userId} type='Create'></EventForm>
-      </div>
+			<div className='wrapper my-8'>
+				<EventForm userId={userId} type='Create'></EventForm>
+			</div>
 		</>
 	);
-}
+};
 
-export default CreateEvent
+export default CreateEvent;
